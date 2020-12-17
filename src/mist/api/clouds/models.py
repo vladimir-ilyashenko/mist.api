0088b38deb5f55ab77c45270c3b972c98e399cc9
mpempekos <spirostza77@hotmail.com>
2020-12-17 16:08:51 +0200

    Pop fail_on_error and fail_on_invalid_params params before calling controller's add method

--- src/mist/api/clouds/models.py
+++ src/mist/api/clouds/models.py
@@ -208,6 +208,8 @@ class Cloud(OwnershipMixin, me.Document):
         cloud = cls(owner=owner, title=title)
         if id:
             cloud.id = id
+        fail_on_error = kwargs.pop('fail_on_error', True)
+        fail_on_invalid_params = kwargs.pop('fail_on_invalid_params', False)
         cloud.ctl.add(user, **kwargs)
         return cloud
 
